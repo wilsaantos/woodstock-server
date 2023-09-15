449124dbf8e6459c6fae54c6627d1d6a408550df
Wilson Santos <wilson.saantos@hotmail.com>
2023-09-15 05:13:07 -0300

    fix deploy

---
+++ dist/config/auth.js
@@ -0,0 +1,9 @@
+// src/config/auth.json
+var secret = "cf56bad86171a8db7e919f5bfe6a2981";
+var auth_default = {
+  secret
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  secret
+});
